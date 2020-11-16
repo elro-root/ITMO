feecbb982a4937e455f8e6659b492f46237122d4
patrikduksin <duksin2018@icloud.com>
2020-11-16 03:41:28 +0300

    task B[is it BST] solved

---
+++ Lab5/B/main.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+
+using namespace std;
+
+/* A binary tree node has data,
+pointer to left child and
+a pointer to right child */
+class node
+{
+public:
+    int data;
+    node* left;
+    node* right;
+
+    /* Constructor that allocates
+    a new node with the given data
+    and NULL left and right pointers. */
+    node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+int isBSTUtil(node* node, int min, int max);
+
+/* Returns true if the given
+tree is a binary search tree
+(efficient version). */
+int isBST(node* node)
+{
+    return(isBSTUtil(node, INT_MIN, INT_MAX));
+}
+
+/* Returns true if the given
+tree is a BST and its values
+are >= min and <= max. */
+int isBSTUtil(node* node, int min, int max)
+{
+    /* an empty tree is BST */
+    if (node==NULL)
+        return 1;
+
+    /* false if this node violates
+    the min/max constraint */
+    if (node->data < min || node->data > max)
+        return 0;
+
+    /* otherwise check the subtrees recursively,
+    tightening the min or max constraint */
+    return
+            isBSTUtil(node->left, min, node->data-1) && // Allow only distinct values
+            isBSTUtil(node->right, node->data+1, max); // Allow only distinct values
+}
+
+
+/* Driver code*/
+int N;
+int main()
+{
+    node *root = new node(2);
+    root->right = new node(-1);
+    root->right->left = new node(3);
+    root->right->right = new node(9);
+
+    if(isBST(root))
+        cout<<"YES";
+    else
+        cout<<"NO";
+
+    return 0;
+}
+
