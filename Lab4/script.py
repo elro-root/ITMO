ba13363046e9ca5ce06b6871f84a1182c3c4afe5
patrikduksin <duksin2018@icloud.com>
2020-11-20 02:32:08 +0300

    Сделана переработка текста и первичные расчеты

---
+++ Lab4/script.py
@@ -0,0 +1,26 @@
+import re, collections
+
+# Сделал ввод из файла brain101.txt и dict1.txt
+file = open('brain101.txt', 'r')
+text = file.read()
+file.close()
+file = open('dict1.txt', 'r')
+dict1 = file.readlines()
+file.close()
+
+out = open('output.txt', 'w')
+text = text.lower()
+text = re.sub(r"[!?,;.:—«()»\n]", " ", text)
+text = text.split()
+count = collections.Counter()
+for i in text:
+    count[i] += 1
+cnt = collections.Counter()
+for i in range(len(dict1)):
+    dict1[i] = re.sub(r"[\n]", "", dict1[i])
+    a = dict1[i].split()
+    cnt.update({a[0]:int(a[1])})
+out.write(f'Количество словоформ в тексте: {str(len(list(count.elements())))}.\n')
+out.write(f'Количество разных словоформ в тексте: {str(len(set(count)))}.\n')
+out.write(f'Количество разных словоформ из моего текста, присутствующих в словаре: {len(set(count) & set(cnt))}.\n')
+out.close()
