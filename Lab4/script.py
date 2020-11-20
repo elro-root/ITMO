e0efa5ee4126d8bcad31f96870dd67fb7f3e0e6a
patrikduksin <duksin2018@icloud.com>
2020-11-20 03:00:59 +0300

    Немного причесал код

--- Lab4/script.py
+++ Lab4/script.py
@@ -8,19 +8,20 @@ file = open('dict1.txt', 'r')
 dict1 = file.readlines()
 file.close()
 
-out = open('output.txt', 'w')
 text = text.lower()
 text = re.sub(r"[!?,;.:—«()»\n]", " ", text)
 text = text.split()
 count = collections.Counter()
 for i in text:
     count[i] += 1
+out = open('output.txt', 'w')
+out.write(f'Количество словоформ в тексте: {str(len(list(count.elements())))}.\n')
+out.write(f'Количество разных словоформ в тексте: {str(len(set(count)))}.\n')
+
 cnt = collections.Counter()
 for i in range(len(dict1)):
     dict1[i] = re.sub(r"[\n]", "", dict1[i])
     a = dict1[i].split()
     cnt.update({a[0]:int(a[1])})
-out.write(f'Количество словоформ в тексте: {str(len(list(count.elements())))}.\n')
-out.write(f'Количество разных словоформ в тексте: {str(len(set(count)))}.\n')
 out.write(f'Количество разных словоформ из моего текста, присутствующих в словаре: {len(set(count) & set(cnt))}.\n')
 out.close()
