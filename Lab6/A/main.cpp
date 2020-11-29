9a4affb86e853a05b30715dd40222731c81a04da
patrikduksin <duksin2018@icloud.com>
2020-11-30 01:16:17 +0300

    task A[set] solved

---
+++ Lab6/A/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#define int long long
+using namespace std;
+
+vector<int> hashTable[1000001];
+void input(){
+    freopen("set.in", "r", stdin);
+    freopen("set.out", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+}
+
+int hashFunc(int element){
+    int hash = abs((element % 1000001));
+    return hash;
+}
+bool exists(int element){
+    int hash = hashFunc(element);
+    if (!(find(hashTable[hash].begin(), hashTable[hash].end(), element) == hashTable[hash].end()))
+        return true;
+    else
+        return false;
+}
+void insert(int element) {
+    if (!exists(element)) {
+        int hash = hashFunc(element);
+
+        if (hashTable[hash].empty()) {
+            new vector<int>(hashTable[hash]);
+            hashTable[hash].push_back(element);
+        } else
+            hashTable[hash].push_back(element);
+    }
+}
+
+void erase(int element){
+    if (exists(element)){
+        int hash = hashFunc(element);
+        hashTable[hash].erase(std::remove(hashTable[hash].begin(), hashTable[hash].end(), element), hashTable[hash].end());
+    }
+}
+
+signed main(){
+    input();
+    string s;
+    int x;
+    while (cin >> s) {
+        if (s == "insert") {
+            cin >> x;
+            insert(x);
+        }
+        if (s == "exists") {
+            cin >> x;
+            if (exists(x))
+                cout << "true" << "\n";
+            else
+                cout << "false" << "\n";
+        }
+        if (s == "delete") {
+            cin >> x;
+            erase(x);
+        }
+    }
+}
