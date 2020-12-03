63c10e4a300e6a46bb4ec319e65a8ddd53755a6e
patrikduksin <duksin2018@icloud.com>
2020-12-03 06:03:11 +0300

    output to file

--- Lab5/script.py
+++ Lab5/script.py
@@ -1,10 +1,10 @@
 import networkx as nx
 graph = nx.DiGraph()
 graph = nx.read_adjlist('graphedges133.txt')
-nx.write_adjlist(graph, 'output.txt')
-print(f'Число ребёр в графе: {nx.number_of_edges(graph)}')
-print(f'Число изолятов в графе: {nx.number_of_isolates(graph)}')
+out = open('output.txt', 'w')
+out.write(f'Число ребёр в графе: {nx.number_of_edges(graph)}\n')
+out.write(f'Число изолятов в графе: {nx.number_of_isolates(graph)}\n')
 degree_sequence = [[n, d] for n, d in graph.degree()]
 degree_sequence.sort(key=lambda x: -x[1])
-print(f"Вершина с самой большой степенью: {degree_sequence[0][0]}, степень: {degree_sequence[0][1]}, "
-      f"список вершин: {' '.join([i[1] for i in list(reversed(list(graph.edges(degree_sequence[0][0]))))])}")
+out.write(f"Вершина с самой большой степенью: {degree_sequence[0][0]}, степень: {degree_sequence[0][1]}, "
+      f"список вершин: {' '.join([i[1] for i in list(reversed(list(graph.edges(degree_sequence[0][0]))))])}\n")
