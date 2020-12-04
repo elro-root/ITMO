b68fcd5804d75c91de75076fd8628b734d96c01f
patrikduksin <duksin2018@icloud.com>
2020-12-04 14:27:52 +0300

    fix isolates again

--- Lab5/script.py
+++ Lab5/script.py
@@ -13,9 +13,11 @@ for i in range(len(edge)):
 
 degree_sequence = [[n, d] for n, d in graph.degree()]
 degree_sequence.sort(key=lambda x: -x[1])
+
 out = open('output.txt', 'w')
 out.write(f'Вопрос 1:\n Число ребёр в графе: {nx.number_of_edges(graph)}\n')
-out.write(f'Вопрос 2:\n Число изолятов в графе: {nx.number_of_isolates(graph)}\n')
+out.write(f'Вопрос 2:\n Число изолятов в графе: {nx.number_of_isolates(graph)}\n'
+          f'    Изоляты: {" ".join(sorted(list(nx.isolates(graph))))}\n')
 out.write(f"Вопрос 3:\n Вершина с самой большой степенью: {degree_sequence[0][0]}, степень: {degree_sequence[0][1]},\n"
-          f"    Вершины: {' '.join([i[1] for i in list(reversed(list(graph.edges(degree_sequence[0][0]))))])}\n")
+          f"    Вершины связанные с {degree_sequence[0][0]}: {' '.join([i[1] for i in list(graph.edges(degree_sequence[0][0]))])}\n")
 out.close()
