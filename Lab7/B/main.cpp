394f4cc643a1b231b5f2142dce8a79341260df39
patrikduksin <duksin2018@icloud.com>
2020-12-14 10:18:12 +0300

    вывод допилен, но почему-то тест 2 неправильный, я ваще ХЗ🤕

--- Lab7/B/main.cpp
+++ Lab7/B/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <queue>
+
 using namespace std;
 
-struct BST {
+struct BST{
     int key{};
-    int height{};
+    unsigned char height{};
     int diff{};
-    size_t number{};
     BST* left = nullptr;
     BST* right = nullptr;
 };
 int N;
 BST *tree;
 
-void height_of_tree(BST *node){
+BST* height_of_tree(BST *node){
     for (int i = N - 1; i >= 0; --i) {
         if (node[i].right) {
             node[i].height = node[i].right->height + 1;
@@ -25,77 +26,105 @@ void height_of_tree(BST *node){
         else
             node[i].height = 1;
     }
+    return node;
 }
 
-void diff(BST *node){
-    for (int i = 0; i < N; ++i) {
-        if (!(node[i].right))
-            if (node[i].left)
-                node[i].diff = node[i].left->height;
-        if (!(node[i].left))
-            if (node[i].right)
-                node[i].diff = node[i].right->height;
-        if (node[i].right && node[i].left)
-            node[i].diff = node[i].right->height - node[i].left->height;
-    }
+unsigned char height(BST* p){
+    return p?p->height:0;
 }
 
-void create_tree(BST *node){
-    int left_son;
-    int right_son;
-    //создание дерева
-    for (int i = 0; i < N; ++i) {
-        cin >> node[i].key >> left_son >> right_son;
-        if (left_son != 0)
-            node[i].left = &node[left_son - 1];
-
-        if (right_son != 0)
-            node[i].right = &node[right_son - 1];
-    }
-    height_of_tree(node);
-    diff(node);
+int diff(BST* p){
+    return height(p->right) - height(p->left);
 }
 
-BST* rotate_left(BST *node){
-    BST *b = node->right;
-    node->right = b->left;
-    b->left = node;
-    height_of_tree(tree);
-    diff(tree);
-    return b->left;
+void fix_height(BST* p){
+    unsigned char hl = height(p->left);
+    unsigned char hr = height(p->right);
+    p->height = (hl>hr?hl:hr)+1;
 }
 
-BST* rotate_right(BST *node){
-    BST *b = node->left;
-    node->left = b->right;
-    b->right = node;
-    height_of_tree(node);
-    diff(node);
-    return b;
+BST* rotate_left(BST* node){
+    BST* p = node->right;
+    node->right = p->left;
+    p->left = node;
+    fix_height(node);
+    fix_height(p);
+    return p;
 }
 
-BST* balance(BST* node){ // балансировка узла node
-    height_of_tree(node);
-    if( node->diff==2 )
+BST* rotate_right(BST* node){
+    BST* q = node->left;
+    node->left = q->right;
+    q->right = node;
+    fix_height(node);
+    fix_height(q);
+    return q;
+}
+
+BST* balance(BST* node){
+    fix_height(node);
+    if(node->diff == 2)
     {
-        if ((node->right->diff) < 0 )
+        if((node->right)->diff < 0)
             node->right = rotate_right(node->right);
         return rotate_left(node);
     }
-    if( node->diff==-2 )
+    if(node->diff == -2)
     {
-        if( (node->left)->diff > 0  )
+        if((node->left)->diff > 0)
             node->left = rotate_left(node->left);
         return rotate_right(node);
     }
-    return node; // балансировка не нужна
+    return node;
 }
 
-
-void key_number(BST *node){
+BST* read(BST* node){
     for (int i = 0; i < N; ++i) {
-        node[i].number = i + 1;
+        int left_c, right_c;
+        cin >> node[i].key >> left_c >> right_c;
+        if (left_c != 0)
+            node[i].left = &node[left_c - 1];
+        if (right_c != 0)
+            node[i].right = &node[right_c - 1];
     }
+    height_of_tree(node);
+    for (int i = 0; i < N; ++i) {
+        node[i].diff = diff(&node[i]);
+    }
+    return node;
+}
+
+int out_bfs(BST *root) {
+    int i = 1;
+    queue<BST*> q;
+
+    if (root) {
+        q.push(root);
+    } else {
+        return 0;
+    }
+    while (!q.empty()) {
+        const BST * const temp_node = q.front();
+        q.pop();
+
+        cout << temp_node->key << " ";
+
+        if (temp_node->left) {
+            q.push(temp_node->left);
+            cout << ++i << " ";
+        } else {
+            cout << "0 ";
+        }
+
+        if (temp_node->right) {
+            q.push(temp_node->right);
+            cout << ++i;
+        } else {
+            cout << "0";
+        }
+        cout << "\n";
+    }
+    return 0;
 }
 
 int main(){
@@ -104,24 +133,11 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie();
     cout.tie();
-
     cin >> N;
     tree = new BST[N];
-    create_tree(tree);
-    key_number(tree);
+    tree = read(tree);
     tree = balance(tree);
-    cout << N << "\n";
-    for (int i = 0; i < N; ++i) {
-        cout << tree[i].key << " ";
-        if (tree[i].left)
-            cout <<tree[i].left->number<< " ";
-        else
-            cout << "0 ";
-        if (tree[i].right)
-            cout << tree[i].right->number << " ";
-        else
-            cout << "0 ";
-        cout <<"\n";
-    }
+    cout << N <<"\n";
+    out_bfs(tree);
     return 0;
 }
