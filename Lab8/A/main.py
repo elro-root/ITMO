d41e3084d4c3ad17a3691b1aea22a637b97e2af7
patrikduksin <duksin2018@icloud.com>
2021-02-19 21:55:36 +0300

    причесал

--- Lab8/A/main.py
+++ Lab8/A/main.py
@@ -1,6 +1,6 @@
 fin = open("input.txt", "r")
 n, m = map(int, fin.readline().split())
-graph = [['0'] * n for i in range(1, n+1)]
+graph = [['0'] * n for i in range(n)]
 for i in range(m):
     a, b = map(int, fin.readline().split())
     graph[a-1][b-1] = '1'
