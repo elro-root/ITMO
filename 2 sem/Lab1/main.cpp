1c871c327c7c0d467d296bbd308713858e434f82
patrikduksin <duksin2018@icloud.com>
2021-03-20 22:35:25 +0300

    init commit for lab1 in 2 sem

---
+++ 2 sem/Lab1/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+class CPoint{
+
+};
+
+class CPolyline{
+
+};
+
+class CClosePolyline{
+
+};
+
+class CPolygon{
+
+};
+
+class CTriangle{
+
+};
+class CTrapezoid{
+
+};
+
+class CRegularPolygon{
+
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
