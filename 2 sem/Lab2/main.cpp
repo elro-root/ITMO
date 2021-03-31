7b9d7dfd62f992750207869201d72ad775a22f06
patrikduksin <duksin2018@icloud.com>
2021-04-01 00:00:13 +0300

    add constructor, destructor, getter and function for print polynomial

--- 2 sem/Lab2/main.cpp
+++ 2 sem/Lab2/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+class CPolynomial{
+public:
+    CPolynomial(std::vector<int> pol){
+        std::reverse(pol.begin(), pol.end());
+        m_polynomial = pol;
+    }
+    ~CPolynomial(){
+        m_polynomial.clear();
+    }
+    std::vector<int> getPolynomial() const {return m_polynomial;}
+    void printPolynomial() const{
+        int size = m_polynomial.size();
+        for (int i = size - 1; i > 0; --i){
+            if (m_polynomial[i] == 1)
+                std::cout <<"x^" << i << "+";
+            else
+                std::cout << m_polynomial[i] << "x^" << i << "+";
+        }
+        std::cout << m_polynomial[0] << std::endl;
+    }
+private:
+    std::vector<int> m_polynomial;
+};
+int main(){
+    CPolynomial polynom = {std::vector<int>{1,2,3,3}};
+    polynom.printPolynomial();
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
