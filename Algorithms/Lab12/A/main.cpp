cc8b3e145e521e08c25ebbe7d4c948205c9899f4
patrikduksin <duksin2018@icloud.com>
2021-04-19 23:20:33 +0300

    ACCEPTED ON 18 FUCKING ATTEMPT!!!!

--- Algorithms/Lab12/A/main.cpp
+++ Algorithms/Lab12/A/main.cpp
@@ -2,41 +2,49 @@
 #include <vector>
 #include <algorithm>
 #include "input.hpp"
-#define long long long
+
+std::vector<long long > findLIS(std::vector<long long > a, unsigned long n) {
+    long long  d[n - 1];
+    long long  prev[n - 1];
+
+    for (long long  i = 0; i < n; ++i) {
+        d[i] = 1;
+        prev[i] = -1;
+        for (long long  j = 0; j < i; ++j)
+            if ( a[j] < a[i] && d[j] + 1 > d[i]) {
+                d[i] = d[j] + 1;
+                prev[i] = j;
+            }
+    }
+    long long  pos = 0;
+    long long  l = d[0];
+    for(long long  i = 0; i < n; ++i){
+        if (d[i] > l) {
+            pos = i;
+            l = d[i];
+        }
+    }
+    std::vector<long long > answer;
+    while(pos != -1) {
+        answer.push_back(a[pos]);
+        pos = prev[pos];
+    }
+    std::reverse(answer.begin(), answer.end());
+    return answer;
+}
+
 int main() {
     input();
-    long n;
+    long long n;
     std::cin >> n;
-    std::vector<long> array(n);
-    std::vector<std::vector<long>> pod_array(n);
-    for (int i = 0; i < n; ++i)
+    std::vector<long long> array(n);
+    std::vector<long long> answer;
+    for (long long  i = 0; i < n; ++i)
         std::cin >> array[i];
-    int g = 0;
-    for (int i = 0; i <= n - 1; ++i) {
-        if (array[i] < array[i + 1]) {
-            pod_array[g].push_back(array[i]);
-        }
-        else {
-            pod_array[g].push_back(array[i]);
-            ++g;
-        }
-    }
-
-    long max = pod_array[0].size();
-    for (auto &i: pod_array) {
-        if (i.size() > max){
-            max = i.size();
-        }
-    }
-    std::cout << max << std::endl;
-    std::reverse(pod_array.begin(), pod_array.end());
-    for (auto &i : pod_array){
-        if(i.size() == max) {
-            for (auto &j: i) {
-                std::cout << j << " ";
-            }
-            return 0;
-        }
+    answer = findLIS(array, n);
+    std::cout << answer.size() << std::endl;
+    for (auto &i : answer){
+        std::cout << i << " ";
     }
     return 0;
 }
