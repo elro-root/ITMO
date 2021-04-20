5bdb9e4897fc1095dae963947961cbb5e15632b8
patrikduksin <duksin2018@icloud.com>
2021-04-20 14:15:14 +0300

    memory limit exceeded, test 30

--- Algorithms/Lab12/C/main.cpp
+++ Algorithms/Lab12/C/main.cpp
@@ -1,8 +1,44 @@
-#include <iostream>
+#include<iostream>
+#include <string>
 #include "input.hpp"
+std::string lcs(std::string X, std::string Y, unsigned long m, unsigned long n ) {
+    int L[m+1][n+1];
+    for (int i = 0; i <= m; ++i) {
+        for (int j = 0; j <= n; ++j) {
+            if (i == 0 || j == 0)
+                L[i][j] = 0;
+            else if (X[i-1] == Y[j-1])
+                L[i][j] = L[i-1][j-1] + 1;
+            else
+                L[i][j] = std::max(L[i-1][j], L[i][j-1]);
+        }
+    }
+    int index = L[m][n];
+    std::string lcs(index, ' ');
+    unsigned long i = m, j = n;
+    while (i > 0 && j > 0) {
+        if (X[i-1] == Y[j-1]) {
+            lcs[index - 1] = X[i-1];
+            i--; j--; index--;
+        }
+            // If not same, then find the larger of two and
+            // go in the direction of larger value
+        else if (L[i-1][j] > L[i][j-1])
+            i--;
+        else
+            j--;
+    }
+    return lcs;
+}
 
-int main() {
+/* Driver program to test above function */
+int main(){
     input();
-    std::cout << "Hello, World!" << std::endl;
+    std::string X, Y;
+    std::cin >> X >> Y;
+    unsigned long m = X.size();
+    unsigned  long n = Y.size();
+    std::string answer = lcs(X, Y, m, n);
+    std::cout << answer << std::endl;
     return 0;
 }
