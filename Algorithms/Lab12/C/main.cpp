364aac2944d3a3204b404885446b00413c14be57
patrikduksin <duksin2018@icloud.com>
2021-04-20 15:51:22 +0300

    wrong answer test 8

--- Algorithms/Lab12/C/main.cpp
+++ Algorithms/Lab12/C/main.cpp
@@ -1,39 +1,43 @@
 #include<iostream>
 #include <string>
-#include "input.hpp"
+//#include "input.hpp"
 std::string lcs(std::string X, std::string Y, unsigned long m, unsigned long n ) {
-    int L[m+1][n+1];
-    for (int i = 0; i <= m; ++i) {
-        for (int j = 0; j <= n; ++j) {
+    int L[2][n + 1];
+    bool bi;
+    for (int i = 0; i <= m; i++) {
+        bi = i & 1;
+        for (int j = 0; j <= n; j++) {
             if (i == 0 || j == 0)
-                L[i][j] = 0;
-            else if (X[i-1] == Y[j-1])
-                L[i][j] = L[i-1][j-1] + 1;
+                L[bi][j] = 0;
+
+            else if (X[i-1] == Y[j-1]){
+                L[bi][j] = L[1 - bi][j - 1] + 1;
+            }
             else
-                L[i][j] = std::max(L[i-1][j], L[i][j-1]);
+                L[bi][j] = std::max(L[1 - bi][j],
+                                    L[bi][j - 1]);
         }
     }
-    int index = L[m][n];
+    int index = L[bi][n];
     std::string lcs(index, ' ');
     unsigned long i = m, j = n;
     while (i > 0 && j > 0) {
         if (X[i-1] == Y[j-1]) {
             lcs[index - 1] = X[i-1];
-            i--; j--; index--;
+            --i; --j; --index;
         }
-            // If not same, then find the larger of two and
-            // go in the direction of larger value
-        else if (L[i-1][j] > L[i][j-1])
-            i--;
+        else if (L[1 - bi][j] > L[bi][j-1])
+            --i;
         else
-            j--;
+            --j;
+        bi = i & 1;
     }
     return lcs;
 }
 
 /* Driver program to test above function */
 int main(){
-    input();
+//    input();
     std::string X, Y;
     std::cin >> X >> Y;
     unsigned long m = X.size();
